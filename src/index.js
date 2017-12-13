4f91b4437702471f79a3ebabb17072df7ae401c1
vcordero07 <vcordero07@gmail.com>
2017-12-12 22:57:37 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './reset.css';
 import './index.css';
-import App from './components/app';
+
+import Game from './components/game';
 
 ReactDOM.render(
-  <App />,
+  <Game />,
   document.getElementById('root')
 );
